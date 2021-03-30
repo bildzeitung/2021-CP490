ce841babbf35928d164200a14fc1341d6cf17d60
bildzeitung <bildzeitung@gmail.com>
2021-03-29 21:57:56 -0400

    Added common module for mixins

---
+++ code/coal-common/coal_common/mixins.py
@@ -0,0 +1,15 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy_utils import UUIDType
+
+
+
+class AttributeMixin:
+    id = Column(UUIDType(binary=False), primary_key=True, default=uuid.uuid4)
+    title = Column(String(64), index=True)
+    value = Column(String(2048))
+    timestamp = Column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
