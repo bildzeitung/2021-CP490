5e91b6374ec1d2775f55bb8857da3e6ce081a891
bildzeitung <bildzeitung@gmail.com>
2021-03-31 14:27:16 -0400

    Build fixes

--- code/mk/wheels.mk
+++ code/mk/wheels.mk
@@ -5,12 +5,14 @@ $(WHEEL_DIR):
 
 wheels: $(VENV) $(PUBLIC_API_SERVER_YAML) $(GAME_SERVER_YAML) $(CONTENT_SERVER_YAML) $(PLAYER_SERVER_YAML) | $(WHEEL_DIR)
 	. $(VENV) && cd public-schema-server && python -m build
+	. $(VENV) && cd coal-common && python -m build
 	. $(VENV) && cd game-server && python -m build
 	. $(VENV) && cd content-server && python -m build
 	. $(VENV) && cd player-server && python -m build
 	. $(VENV) && cd connexion && python -m build
 	cp connexion/dist/*.whl $(WHEEL_DIR)
 	cp public-schema-server/dist/coal_public_api_server-*.whl $(WHEEL_DIR)
+	cp coal-common/dist/coal_*.whl $(WHEEL_DIR)
 	cp game-server/dist/coal_*.whl $(WHEEL_DIR)
 	cp content-server/dist/coal_*.whl $(WHEEL_DIR)
 	cp player-server/dist/coal_*.whl $(WHEEL_DIR)
